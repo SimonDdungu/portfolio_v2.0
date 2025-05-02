2f54ad676c17927e15276a374efe8389ecae795c
Simon Ddungu <dev.simonddungu@gmail.com>
2025-05-03 01:57:44 +0300

    Added Home Page

---
+++ src/pages/Home_page.tsx
@@ -0,0 +1,14 @@
+import { About } from "@/components/About"
+import { Hero } from "@/components/Hero"
+import { Projects } from "@/components/Projects"
+const Home_page = () => {
+  return (
+    <>
+        <Hero />
+        <About />
+        <Projects />
+    </>
+  )
+}
+
+export default Home_page
