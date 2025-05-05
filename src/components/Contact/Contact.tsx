ab8df53af6a48f96d36e87c8a909afc82d021837
Simon Ddungu <dev.simonddungu@gmail.com>
2025-05-05 20:45:15 +0300

    Added Contact components

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <section id="contact" className='py-20 px-5 bg-gray-950'>
+        <div>
+            
+        </div>
+    </section>
+  )
+}
+
+export default Contact
