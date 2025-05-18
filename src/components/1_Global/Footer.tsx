915ee6fc8d5831972bcc497247ad18c8694443c0
Simon Ddungu <dev.simonddungu@gmail.com>
2025-05-18 19:16:53 +0300

    Added Footer

---
+++ src/components/1_Global/Footer.tsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+  return (
+    <section className="py-5 px-5 bg-gray-900">
+        <div>
+            <span className="block text-center text-sm font-light tracking-wider cursor-default">Designed & Developed by Simon Ddungu</span>
+        </div>
+    </section>
+  )
+}
+
+export default Footer
