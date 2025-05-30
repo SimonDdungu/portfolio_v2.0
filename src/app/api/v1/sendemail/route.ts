41b96425b53a4e1c08c9f7c8ee79b4a84c810cd8
SimonDdungu <dev.simonddungu@example.com>
2025-05-30 21:56:45 +0300

    Fix #1

--- src/app/api/v1/sendemail/route.ts
+++ src/app/api/v1/sendemail/route.ts
@@ -3,23 +3,20 @@ import UserEmail from "@/components/Contact/Email/UserEmail";
 import { Resend } from "resend";
 
 
-console.log("HITTING THIS API")
-console.log(process.env.EMAIL_KEY)
-
-const resend = new Resend(process.env.EMAIL_KEY)
 
 
 export async function POST(req: NextRequest){
     console.log("HITTING THIS API")
     console.log(process.env.EMAIL_KEY)
-
+    
     const data = await req.json()
-
+    
     const {first_name, last_name, email, message } = data;
-
+    
     console.log('Received:', { first_name, last_name, email, message });
-
+    
     try {
+        const resend = new Resend(process.env.EMAIL_KEY)
         const sendEmail = await resend.emails.send({
             from: `dev.simonddungu@gmail.com`,
             to: email,
