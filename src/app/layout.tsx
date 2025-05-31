8e3fcfaf77e310af0783d59b90dd1ed908a0a828
SimonDdungu <dev.simonddungu@example.com>
2025-05-31 19:25:00 +0300

    Added Radix Ui and Dialog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Footer, Navbar } from "@/components/1_Global";
 
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,9 +32,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <main className="pt-16">
-          {children}
-        </main>
+        <Theme>
+          <main className="pt-16">
+            {children}
+          </main>
+        </Theme>
         <Footer />
       </body>
     </html>
---
+++ src/components/1_Global/EmailSuccessDialog.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import * as Dialog from '@radix-ui/react-dialog';
+import { BiX } from 'react-icons/bi';
+
+interface MyDialog {
+    isDialogOpen: boolean,
+    onOpenChange: (ourSetFunction: boolean) => void,
+    message: string
+}
+
+
+const EmailSuccessDialog = ({ isDialogOpen, onOpenChange, message }: MyDialog) => {
+    return (
+        <div>
+            <Dialog.Root open={isDialogOpen} onOpenChange={onOpenChange}>
+
+                <Dialog.Portal>
+
+                    <Dialog.Overlay className="fixed inset-0 bg-black/50 flex items-center justify-center z-[1000]" />
+
+                    <Dialog.Content className="fixed z-90 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-900 p-8 rounded-lg shadow-xl text-center max-w-sm w-[90%] animate-in fade-in zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=closed]:zoom-out-95 duration-200">
+
+                        <Dialog.Description className="text-lg text-white mb-6">
+                            {message}
+                        </Dialog.Description>
+
+
+                        {/*Close Buttons*/}
+                        <Dialog.Close>
+                            <button className='rounded-lg block w-max mx-auto text-sm px-4 py-2 bg-white text-black cursor-pointer
+                            hover:bg-gray-300 transition-all ease-in duration-200'>
+                                Okay
+                            </button>
+                        </Dialog.Close>
+
+                        <Dialog.Close>
+                            <BiX className="text-3xl absolute top-2 right-2 p-2" />
+                        </Dialog.Close>
+
+                    </Dialog.Content>
+                </Dialog.Portal>
+            </Dialog.Root>
+        </div>
+    )
+}
+
+export default EmailSuccessDialog;
