267138ebb9275d3b755008032e6a8044c704229f
SimonDdungu <dev.simonddungu@example.com>
2025-06-01 20:43:42 +0300

    Added Fav icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Simon's Portfolio" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
